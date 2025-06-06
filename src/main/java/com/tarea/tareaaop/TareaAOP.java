5b3bb91f931ef1b9099bfe6ff79021a44cc1b661
XavierG <ixavierguzmang05@gmail.com>
2025-06-06 16:45:37 -0500

    first commit

---
+++ src/main/java/com/tarea/tareaaop/TareaAOP.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.tarea.tareaaop;
+
+/**
+ *
+ * @author Nueva
+ */
+public class TareaAOP {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
